a0b8c7536a1bf0ffd304c33aa18b195c1820920d
David Horwitz <dhorwitz@gmail.com>
2017-03-26 16:18:33 +0200

    photo, Afr, xH lang

--- api/src/java/za/co/davidhorwitz/stl/api/model/STLCategories.java
+++ api/src/java/za/co/davidhorwitz/stl/api/model/STLCategories.java
@@ -1,20 +1,27 @@
 package za.co.davidhorwitz.stl.api.model;
 
+/**
+ * @author dhorwitz
+ *
+ */
 public class STLCategories {
 	private Long id;
 	private String categories;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getCategories() {
 		return categories;
 	}
+
 	public void setCategories(String categories) {
 		this.categories = categories;
 	}
-	
+
 }
--- tool/src/java/za/co/davidhorwitz/stl/producers/MainViewProducer.java
+++ tool/src/java/za/co/davidhorwitz/stl/producers/MainViewProducer.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import uk.org.ponder.messageutil.MessageLocator;
 import uk.org.ponder.rsf.components.UIContainer;
 import uk.org.ponder.rsf.components.UILink;
@@ -20,26 +19,28 @@ import za.co.davidhorwitz.stl.api.model.STLPhoto;
 import za.co.davidhorwitz.stl.api.model.STLTest;
 import za.co.davidhorwitz.stl.renderers.NavBarRenderer;
 
-public class MainViewProducer  implements ViewComponentProducer, DefaultView{
+public class MainViewProducer implements ViewComponentProducer, DefaultView {
 
 	private static Logger log = LoggerFactory.getLogger(MainViewProducer.class);
 	public static final String VIEW_ID = "Index";
-	
+
 	private MessageLocator messageLocator;
+
 	public void setMessageLocator(MessageLocator messageLocator) {
 		this.messageLocator = messageLocator;
 	}
-	
+
 	private STLLogic stlLogic;
+
 	public void setStlLogic(STLLogic stlLogic) {
 		this.stlLogic = stlLogic;
 	}
-	
+
 	private NavBarRenderer navBarRenderer;
+
 	public void setNavBarRenderer(NavBarRenderer navBarRenderer) {
 		this.navBarRenderer = navBarRenderer;
 	}
-
 
 	@Override
 	public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
@@ -53,49 +54,50 @@ public class MainViewProducer  implements ViewComponentProducer, DefaultView{
 		navBarRenderer.makeNavBar(tofill, "navIntraTool:", VIEW_ID);
 
 		List<STLPhoto> listPhoto = test.getPhoto();
-		STLPhoto photo = null;
-		String photoLing = null;
+		
+		
 		log.debug("mest: " + test.getMath() + "; listPhoto: " + listPhoto.size());
-		for (int q = 0; q < listPhoto.size(); q++) {
-			photo = listPhoto.get(q); // stlLogic.getSTLPhoto(); //
-			log.debug("photo: " + photo.getId() + " " + photo.getText() + " " + photo.getaThis());
+		int i = test.getMath() -1;
+		STLPhoto photo = listPhoto.get(i); // stlLogic.getSTLPhoto(); //
+		log.debug("photo: " + photo.getId() + " " + photo.getText() + " " + photo.getaThis());
 
-			photoLing = stlLogic.getPhotoContent() + photo.getId().toString() + ".jpg";
-			log.debug("Link: " + photoLing);
-			if (listPhoto.size() <= test.getMath()) {
-				break;
-			}
-
-		}
-		UILink.make(tofill, "photo", photoLing);
+		String photoLing = stlLogic.getPhotoContent() + photo.getId().toString() + ".jpg";
+		log.debug("Link: " + photoLing);
+		if (photo.isHeigh()) {
+			UILink.make(tofill, "photo2", photoLing);
+		} else {
+			UILink.make(tofill, "photo", photoLing);
+		} 
 		UIOutput.make(tofill, "text", photo.getText() + " " + getText(photo.getaThis()), photo.getText());
 
-		if (test.getMath() == 2) {
+		if (test.getMath() >= 2) {
 			UIMessage.make(tofill, "previous1", "img_previous");
 		}
 
-		UIMessage.make(tofill, "next1", "img_next");
+		// next
+		log.debug("next: " + test.getPess() + " != " + test.getMath());
+		if (test.getPess() != test.getMath()) {
+			int next = test.getMath() + 1;
+			log.debug("next: " + next);
+			test.setMath(next);
+			UIMessage.make(tofill, "next1", "img_next");
+		}
 
 		UIOutput.make(tofill, "cont", test.getMath() + "/" + test.getPess());
 
-		// next
-		int next = test.getMath() + 1;
-		log.debug("next: " + next);
-		test.setMath(next);
-
 	}
 
-	
 	@Override
 	public String getViewID() {
 		return VIEW_ID;
 	}
+
 	private String getText(String t) {
 		String text = t;
 		int i = text.length();
 		int q = 1;
-		 log.debug("text: " + i);
-		 
+		log.debug("text: " + i);
+
 		String c = new String("_");
 		while (i != q) {
 			c = c.concat("_");
